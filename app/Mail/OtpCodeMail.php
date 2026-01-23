be7de18c29701d4edff4c295d9dd25221e65d538
LuisD <luisodangelo@gmail.com>
2026-01-23 15:16:24 -0300

    Add OtpCodeMail mailer (PSR-4)

---
+++ app/Mail/OtpCodeMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OtpCodeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public string $code,
+        public string $purpose = 'first_access',
+        public string $name = ''
+    ) {}
+
+    public function build()
+    {
+        $subject = $this->purpose === 'password_reset'
+            ? 'Código para recuperar contraseña'
+            : 'Código de primer acceso';
+
+        return $this->subject($subject)
+            ->view('emails.otp_code')
+            ->with([
+                'code' => $this->code,
+                'purpose' => $this->purpose,
+                'name' => $this->name,
+                'ttl' => '10 minutos',
+            ]);
+    }
+}
